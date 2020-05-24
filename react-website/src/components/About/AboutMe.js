9d2ecf62c78552d08e4f554af84072da62f4f150
pravi34 <prernaravi@gatech.edu>
2020-05-24 13:27:56 -0700

    Profile pic added

--- react-website/src/components/About/AboutMe.js
+++ react-website/src/components/About/AboutMe.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {Navbar, Image, Nav, Container} from 'react-bootstrap';
+import {Col, Image, Row, Container} from 'react-bootstrap';
+import profile from "../../images/profile-pic.JPG";
 
 class AboutMe extends React.Component{
     render () {
         return (
             <Container className="about-me">
+                <Col xs={5}>
 
+                
+                <Image responsive className="profile-pic-format" src={profile} fluid="true"/>
+                </Col>
             </Container>
         );
     }
