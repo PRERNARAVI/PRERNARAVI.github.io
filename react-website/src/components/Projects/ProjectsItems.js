bcb2ca7ab121a6ab48e4d623ad8c97c2b2aad807
pravi34 <prernaravi@gatech.edu>
2020-08-13 03:06:16 -0700

    Added titles for projects

--- react-website/src/components/Projects/ProjectsItems.js
+++ react-website/src/components/Projects/ProjectsItems.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Col, Image, Row, Container, Card} from 'react-bootstrap';
 import { HashRouter as Router, Switch, Route, NavLink } from "react-router-dom";
-import MSFT from "../../images/MSFT-Project.png";
+import MSFT from "../../images/MSFT-Project.jpg";
 import Journaling from "../../images/Journaling-Project.jpg";
 import Library from "../../images/Library.jpeg";
 import Connect from "../../images/Connect-Project.jpeg";
@@ -23,15 +23,24 @@ class ProjectsItems extends React.Component{
                             style={{marginTop: "120px", border: "none",
                             borderRadius: "10px", height: "40vh"}}>
                         <div className="card-image" >
-                            <Card.Img style={{height: "40vh"}}
+                            <Card.Img style={{height: "40vh", opacity: 0.6}}
                                  src={Library} alt="Card image" />
-                            <Card.ImgOverlay>
-                                <Card.Title>Card title</Card.Title>
-                                <Card.Text>
+                            <Card.ImgOverlay >
+                                <Card.Title 
+                                    style={{
+                                        fontFamily: "Tahoma, Geneva, sans-serif", 
+                                        fontWeight: "normal",
+                                        fontSize: 40,
+                                        margin: "auto",
+                                        paddingTop: "100px",
+                                    }}>SCHOOL LIBRARY HUB <br></br>
+                                    FOR <br></br>MICROSOFT TEAMS
+                                </Card.Title>
+                                {/* <Card.Text>
                                 This is a wider card with supporting text below as a natural lead-in to
                                 additional content. This content is a little bit longer.
                                 </Card.Text>
-                                <Card.Text>Last updated 3 mins ago</Card.Text>
+                                <Card.Text>Last updated 3 mins ago</Card.Text> */}
                             </Card.ImgOverlay>
                         </div>
                         </Card>
@@ -39,13 +48,44 @@ class ProjectsItems extends React.Component{
                     
                 </Col>
                 <Col xs={6}>
-                    <a href="#">
+                <a href="#">
                         <Card className="bg-dark text-white" 
                             style={{marginTop: "120px", border: "none",
                             borderRadius: "10px", height: "40vh"}}>
-                        <div className="card-image" href="#">
+                        <div className="card-image" >
+                            <Card.Img style={{height: "40vh", opacity: 0.8}}
+                                 src={ND} alt="Card image" />
+                            <Card.ImgOverlay >
+                                <Card.Title 
+                                    style={{
+                                        fontFamily: "Tahoma, Geneva, sans-serif", 
+                                        fontWeight: "normal",
+                                        fontSize: 40,
+                                        margin: "auto",
+                                        paddingTop: "120px",
+                                    }}>understaNDing <br></br>
+                                    NEURODIVERISTY
+                                </Card.Title>
+                                {/* <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+                                </Card.Text>
+                                <Card.Text>Last updated 3 mins ago</Card.Text> */}
+                            </Card.ImgOverlay>
+                        </div>
+                        </Card>
+                    </a>
+                </Col>
+                </Row>
+                <Row>
+                <Col >
+                    <a href="#">
+                        <Card className="bg-dark text-white" 
+                            style={{marginTop: "40px", border: "none",
+                            borderRadius: "10px", height: "40vh"}}>
+                        <div className="card-image" >
                             <Card.Img style={{height: "40vh"}}
-                                src={ND} alt="Card image" />
+                                 src={MSFT} alt="Card image" />
                             <Card.ImgOverlay>
                                 <Card.Title>Card title</Card.Title>
                                 <Card.Text>
@@ -58,8 +98,6 @@ class ProjectsItems extends React.Component{
                         </Card>
                     </a>
                 </Col>
-                </Row>
-                <Row>
                 <Col >
                     <a href="#">
                         <Card className="bg-dark text-white" 
@@ -80,36 +118,16 @@ class ProjectsItems extends React.Component{
                         </Card>
                     </a>
                 </Col>
-                <Col >
-                    <a href="#">
-                        <Card className="bg-dark text-white" 
-                            style={{marginTop: "40px", border: "none",
-                            borderRadius: "10px", height: "40vh"}}>
-                        <div className="card-image" >
-                            <Card.Img style={{height: "40vh"}}
-                                 src={Connect} alt="Card image" />
-                            <Card.ImgOverlay>
-                                <Card.Title>Card title</Card.Title>
-                                <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
-                                </Card.Text>
-                                <Card.Text>Last updated 3 mins ago</Card.Text>
-                            </Card.ImgOverlay>
-                        </div>
-                        </Card>
-                    </a>
-                </Col>
                 </Row>
                 <Row>
                 <Col >
                     <a href="#">
                         <Card className="bg-dark text-white" 
-                            style={{marginTop: "40px", border: "none",
+                            style={{marginTop: "40px", marginBottom: "40px", border: "none",
                             borderRadius: "10px", height: "40vh"}}>
                         <div className="card-image" >
                             <Card.Img style={{height: "40vh"}}
-                                 src={Drop} alt="Card image" />
+                                 src={Connect} alt="Card image" />
                             <Card.ImgOverlay>
                                 <Card.Title>Card title</Card.Title>
                                 <Card.Text>
@@ -125,11 +143,11 @@ class ProjectsItems extends React.Component{
                 <Col >
                     <a href="#">
                         <Card className="bg-dark text-white" 
-                            style={{marginTop: "40px", border: "none",
+                            style={{marginTop: "40px", marginBottom: "40px", border: "none",
                             borderRadius: "10px", height: "40vh"}}>
                         <div className="card-image" >
                             <Card.Img style={{height: "40vh"}}
-                                 src={Connect} alt="Card image" />
+                                 src={Drop} alt="Card image" />
                             <Card.ImgOverlay>
                                 <Card.Title>Card title</Card.Title>
                                 <Card.Text>
