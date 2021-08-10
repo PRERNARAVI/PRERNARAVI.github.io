0926f7aa3f1b594bc41adeaa8b270eb852b0f734
PRERNARAVI <prernaravi2k@gmail.com>
2021-08-10 22:38:46 +0400

    Research publications tab complete

--- react-website/src/components/Research/ResearchItems.js
+++ react-website/src/components/Research/ResearchItems.js
@@ -21,6 +21,7 @@ function ASLModal(props) {
         fontFamily: "Tahoma, Geneva, sans-serif",
     }
     return (
+    <div onClick={e => e.stopPropagation()}>
       <Modal
         {...props}
         size="xl"
@@ -102,17 +103,20 @@ function ASLModal(props) {
           <Button style={{backgroundColor: "black", border: 0, color: "#E9C46A", fontFamily: "Tahoma, Geneva, sans-serif"}} onClick={props.onHide}>Close Project</Button>
         </Modal.Footer>
       </Modal>
+      </div>
     );
   }
 
-    function TandemModal(propsTwo) {
+function TandemModal(props) {
     var fontFormat = {
         fontFamily: "Tahoma, Geneva, sans-serif",
     }
     return (
+    <div onClick={e => e.stopPropagation()}>
       <Modal
-        {...propsTwo}
+        {...props}
         size="xl"
+        keyboard
         dialogClassName="modal-90w"
         aria-labelledby="contained-modal-title-vcenter-two"
         scrollable
@@ -165,11 +169,13 @@ function ASLModal(props) {
         </Container>
         </Modal.Body>
         <Modal.Footer style={{backgroundColor: "black", border: 0}}>
-          <Button style={{backgroundColor: "black", border: 0, color: "#E9C46A", fontFamily: "Tahoma, Geneva, sans-serif"}} onClick={propsTwo.onHide}>Close Project</Button>
+          <Button style={{backgroundColor: "black", border: 0, color: "#E9C46A", fontFamily: "Tahoma, Geneva, sans-serif"}} onClick={props.onHide}>Close Project</Button>
         </Modal.Footer>
       </Modal>
+      </div>
     );
   }
+
 
 function Research() {
     const [modalShowASL, setModalShowASL] = React.useState(false);
@@ -178,7 +184,7 @@ function Research() {
     return (
         <Container style={{marginLeft: "70px"}}>
             <Row style={{marginRight: "0px"}}>
-            {/** ASL ITEM*/}
+            {/** *********************************ASL ITEM************** */}
             <Col md={10}>
             <Row>
             <Col >
@@ -219,8 +225,8 @@ function Research() {
                     />
                 </a>  
             </Col>
-
-        <Col >
+            {/** *********************************TANDEM ITEM************** */}
+            <Col >
                 <a role="button" style={{cursor: "pointer"}} onClick={() => setModalShowTandem(true)}>      
                     <Card className="bg-dark text-white" 
                         style={{marginTop: "120px", border: "none",
@@ -263,6 +269,8 @@ function Research() {
             {/** ROW 2 */}
 
             <Row style={{marginTop: "-60px", paddingBottom: "50px"}} >
+
+             {/** *********************************CAT ITEM************** */}   
             <Col >
                 <a role="button" style={{cursor: "pointer"}} onClick={() => setModalShowASL(true)}>      
                     <Card className="bg-dark text-white" 
@@ -278,18 +286,18 @@ function Research() {
                                 paddingBottom: '0px'
                             }}> 
                             <p style={{lineHeight: "1.5", color: "#E9C46A"}}>
-                                &ensp;CopyCat: Using Sign Language Recognition to Help Deaf Children Acquire Language Skills
+                                &ensp;Co-Designing Parent-Education ICTs with Hispanic Immigrants and Education System
                             </p>
                         </Card.Header>
                        
-                        <ZoomIn>
-                        <Card.Img style={{height: "40vh"}}
-                             src="https://www.verywellhealth.com/thmb/_u2YBR9a2OcAihIAcTGV8wsrmlY=/2556x1917/smart/filters:no_upscale()/gettyimages-495596293-5a2192db5b6e24001a612abe.jpg" alt="Card image" />
+                        <ZoomIn >
+                        <Card.Img style={{height: "45vh", opacity: "0.55"}}
+                             src="http://marisolvillacres.website/images/research/familia.png" alt="Card image" />
                         </ZoomIn>
                         
                             <Card.Text style={{fontSize: 16, margin: '10px'}}>
-                            Advisor: Dr. Thad Starner <br/>
-                            GT Ubiquitous Computing (Ubicomp) Lab
+                            Advisor: Dr. Betsy Disalvo <br/>
+                            GT Culture and Technology (CAT) Lab
                             </Card.Text>
                             <Card.Footer><a href="#" style={{color: "#E9C46A"}} onClick={() => setModalShowASL(true)}>Read More</a></Card.Footer>
 
@@ -317,13 +325,13 @@ function Research() {
                                 paddingBottom: '0px'
                             }}> 
                             <p style={{lineHeight: "1.5", color: "#E9C46A"}}>
-                                &ensp;The COVID-19 Pandemic Shift to Remote Learning under Resource Constraints
+                                &ensp;Avanti Fellows - Ledby Girls' Leadership and Mentorship Program
                             </p>
                         </Card.Header>
                        
                         <ZoomIn>
-                        <Card.Img style={{height: "41vh", boxShadow: "60px 60px 100px 100px #000000;", border: 0}}
-                             src="https://static.reuters.com/resources/r/?m=02&d=20200320&t=2&i=1505762610&r=LYNXMPEG2J0KV&w=800" alt="Card image" />
+                        <Card.Img style={{height: "42vh", boxShadow: "60px 60px 100px 100px #000000;", border: 0, opacity: "0.55"}}
+                             src="https://www.brookings.edu/wp-content/uploads/2017/09/cue_reaching-for-the-sky_001.jpg" alt="Card image" />
                         </ZoomIn>
                         
                             <Card.Text style={{fontSize: 16, margin: '10px'}}>
@@ -349,7 +357,7 @@ function Research() {
 
             
        <Col style={{marginTop: "100px"}}>
-            <Container style={{backgroundColor: "black", marginTop: "20px", width: "245%", height: "148vh"}}>
+            <Container style={{backgroundColor: "black", marginTop: "20px", width: "240%", height: "148vh"}}>
             <br/>
             <div style={{fontSize: 22, color: "#E9C46A"}}>Publications</div>
             <br/>
@@ -368,6 +376,18 @@ function Research() {
             <div style={{marginTop: "10px"}}>Leveraging AlphaPose as a 2-D Pose Estimator for American Sign Language Recognition</div></p>
             <p style={{color: "#f2ebdd", fontSize: 15, textAlign: "left", marginLeft: "10px"}}><b style={{color: "#E9C46A"}}>Prerna Ravi</b> (Advisor: Dr. Thad Starner)</p>
             <p style={{color: "#E9C46A", fontSize: 15, textAlign: "left", marginLeft: "10px"}}>Bachelor's Thesis
+            </p>
+            <div style={{fontSize: 20, color: "#E9C46A", borderTop: "2px dotted #E9C46A"}}><br/>Presentations</div>
+            <br/>
+            <p style={{marginLeft: "10px", textAlign: "left", color: "#E9C46A", fontSize: 17}}>
+            <div style={{marginTop: "10px"}}>CopyCat: Intuitive Calibration and Data Collection Procedures for ASL Recognition using Azure Kinect</div></p>
+            <p style={{color: "#f2ebdd", fontSize: 15, textAlign: "left", marginLeft: "10px"}}><b style={{color: "#E9C46A"}}>Prerna Ravi </b>and Thad Starner</p>
+            <p style={{color: "#E9C46A", fontSize: 15, textAlign: "left", marginLeft: "10px"}}>Undergraduate Research Symposium at Georgia Tech 2019
+            </p>
+            <p style={{marginLeft: "10px", textAlign: "left", color: "#E9C46A", fontSize: 17, borderTop: "2px dotted #E9C46A"}}>
+            <div style={{marginTop: "10px"}}>CopyCat: Pose Estimation for ASL Recognition using OpenPose and Microsoft Kinect</div></p>
+            <p style={{color: "#f2ebdd", fontSize: 15, textAlign: "left", marginLeft: "10px"}}><b style={{color: "#E9C46A"}}>Prerna Ravi </b>, Peter McAughan, Theo Panagiotopoulos, Dhruva Bansal, and Thad Starner</p>
+            <p style={{color: "#E9C46A", fontSize: 15, textAlign: "left", marginLeft: "10px"}}>Georgia Tech GVU Center Research Showcase 2019
             </p>
             {/* <br/>
             <div style={{fontSize: 20, color: "#E9C46A"}}>Presentations</div> */}
