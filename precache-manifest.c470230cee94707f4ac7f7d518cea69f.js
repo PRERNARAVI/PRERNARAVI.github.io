b604062ad233f0b54ca7737ac84d05a9794f9a01
PRERNARAVI <prernaravi2k@gmail.com>
2021-09-08 23:22:43 -0400

    Updates

---
+++ precache-manifest.c470230cee94707f4ac7f7d518cea69f.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3f976c8812eb70fcc868f0a92f2407",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32c687a2d9828b196c50",
+    "url": "/static/css/main.395eba02.chunk.css"
+  },
+  {
+    "revision": "feb2c8b67799b311672b",
+    "url": "/static/js/2.f63ab0c0.chunk.js"
+  },
+  {
+    "revision": "90272e395f30388283854af7d206f76a",
+    "url": "/static/js/2.f63ab0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32c687a2d9828b196c50",
+    "url": "/static/js/main.afd0cd43.chunk.js"
+  },
+  {
+    "revision": "60fa70ef6557751e5513",
+    "url": "/static/js/runtime-main.7a1ad14e.js"
+  },
+  {
+    "revision": "fd84ee878caccb66aedb144fb39905a8",
+    "url": "/static/media/ASL.fd84ee87.png"
+  },
+  {
+    "revision": "c017a9e3944f67a9d11128e6f15174fb",
+    "url": "/static/media/Adobe.c017a9e3.jpeg"
+  },
+  {
+    "revision": "08871ef52fc0b92ea75fc3c60f371e2b",
+    "url": "/static/media/Art.08871ef5.jpg"
+  },
+  {
+    "revision": "d576446f1bd1e6ffe777107819ac63a6",
+    "url": "/static/media/BlackOne.d576446f.jpg"
+  },
+  {
+    "revision": "fb35c69dc16ec0de171a1a1dddfc2ced",
+    "url": "/static/media/Book.fb35c69d.png"
+  },
+  {
+    "revision": "34c08b34397a19e0688358ca7f45d2bc",
+    "url": "/static/media/CHI_Logo.34c08b34.png"
+  },
+  {
+    "revision": "9a2857ad48fa3677db3adafb02c23c7d",
+    "url": "/static/media/CSCW.9a2857ad.png"
+  },
+  {
+    "revision": "cdbf48e05cb29d7839fb9f5565341c2e",
+    "url": "/static/media/CocAward2020.cdbf48e0.jpeg"
+  },
+  {
+    "revision": "38e07d0fd166e6e11e03d3bd38dbae80",
+    "url": "/static/media/CocAward2021.38e07d0f.jpeg"
+  },
+  {
+    "revision": "ccb6699661b4b47669eab045a75f296f",
+    "url": "/static/media/Connect-Project.ccb66996.jpeg"
+  },
+  {
+    "revision": "41e931ee2294c89169b5c8e409f302b9",
+    "url": "/static/media/Connect.41e931ee.png"
+  },
+  {
+    "revision": "cdaa10800acc40f77e8f2610eed26bc4",
+    "url": "/static/media/CopyCat_Stats.cdaa1080.png"
+  },
+  {
+    "revision": "a08e4e3d7163d86f6582df3b793f57ec",
+    "url": "/static/media/Dance.a08e4e3d.jpg"
+  },
+  {
+    "revision": "137eee572c3ee75beccc1f85bcd1a99c",
+    "url": "/static/media/DaybookScreens.137eee57.png"
+  },
+  {
+    "revision": "228c10c87ae66014a8b17d1e4cea1fa9",
+    "url": "/static/media/Deep.228c10c8.png"
+  },
+  {
+    "revision": "bf3fbce003e4f462783cddf62fb09732",
+    "url": "/static/media/Drop.bf3fbce0.jpg"
+  },
+  {
+    "revision": "6312f241f232649c8a66387cfc087a77",
+    "url": "/static/media/DropSecond.6312f241.png"
+  },
+  {
+    "revision": "14d405b6b0a30331bdad56080dcd8d3d",
+    "url": "/static/media/ExperienceWallpaper.14d405b6.jpg"
+  },
+  {
+    "revision": "afd732b05c5e75021411de5e0368da73",
+    "url": "/static/media/Journaling-Project.afd732b0.jpg"
+  },
+  {
+    "revision": "7213f96d6876082a0225502198a54542",
+    "url": "/static/media/Laptop.7213f96d.jpg"
+  },
+  {
+    "revision": "e9aa8f2cba0a4c2f604b734b414967a5",
+    "url": "/static/media/Library.e9aa8f2c.jpeg"
+  },
+  {
+    "revision": "90ba0fcc2afef8622aee22c700e5139e",
+    "url": "/static/media/MSFT-Project.90ba0fcc.jpg"
+  },
+  {
+    "revision": "bc295d95a9b333987c819b2f6de37c28",
+    "url": "/static/media/Mentor.bc295d95.png"
+  },
+  {
+    "revision": "418563d9c9da735c3765a1a73e38617d",
+    "url": "/static/media/ND.418563d9.jpg"
+  },
+  {
+    "revision": "d057d0c211780fb3b4b4a05bf9ecaed8",
+    "url": "/static/media/NDDiversity.d057d0c2.png"
+  },
+  {
+    "revision": "547aee37a793f5dd0eff42f71d8ad556",
+    "url": "/static/media/PRERNA_RAVI_CV.547aee37.pdf"
+  },
+  {
+    "revision": "cba019d3c6846f542b36577c5525ef4d",
+    "url": "/static/media/PURA.cba019d3.png"
+  },
+  {
+    "revision": "7301ac8d412fa9c4136918a683b3b17a",
+    "url": "/static/media/PopUpBg.7301ac8d.jpg"
+  },
+  {
+    "revision": "7ef8e1a790b8ece426f3f4776bbe36be",
+    "url": "/static/media/SchoolLibrary.7ef8e1a7.png"
+  },
+  {
+    "revision": "79d7fd902b8fb924c63db414c7422ee6",
+    "url": "/static/media/Survey.79d7fd90.png"
+  },
+  {
+    "revision": "183ec4adc6bc9c0ebaf705a315c89e09",
+    "url": "/static/media/TANDEM.183ec4ad.jpg"
+  },
+  {
+    "revision": "8326bfee26b367f8c078a25bf4ab76b4",
+    "url": "/static/media/black.8326bfee.jpg"
+  },
+  {
+    "revision": "da764abe4f3b3e8c9bc365639e75ba17",
+    "url": "/static/media/hands.da764abe.png"
+  },
+  {
+    "revision": "26fcb722357cfa247b92982d46c0af51",
+    "url": "/static/media/home-page-wallpaper.26fcb722.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c53024d3841ace2642332d28d772ea6b",
+    "url": "/static/media/meTwo.c53024d3.jpg"
+  },
+  {
+    "revision": "93ecdb34383f8d2e374ec78ba18f8286",
+    "url": "/static/media/profile-pic.93ecdb34.JPG"
+  },
+  {
+    "revision": "b16f5b102c4ea6091f0722d724a0c5b0",
+    "url": "/static/media/research_wallpaper.b16f5b10.jpg"
+  },
+  {
+    "revision": "d3c72c9956c41718096979a018d45743",
+    "url": "/static/media/ubicomp.d3c72c99.png"
+  }
+]);
